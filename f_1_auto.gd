6c35633648aec5de4be52e891b8f30be571e1b72
Adricheri1 <adiwie.gens@gmail.com>
2025-07-11 23:02:17 +0200

    F1 Auto

---
+++ f_1_auto.gd
@@ -0,0 +1,44 @@
+extends RigidBody2D
+
+# Variablen
+@export var acceleration := 3000.0
+@export var max_speed := 1000.0
+@export var steering_speed := 3.0
+@export var grip := 10.0
+@export var downforce := 20.0
+
+@export var speed_label_path : NodePath  # Zeigt auf das Label
+@onready var speed_label := get_node(speed_label_path)
+
+
+var input_accel := 0.0
+var input_steer := 0.0
+
+
+func _physics_process(delta):
+	input_accel = Input.get_action_strength("ui_up") - Input.get_action_strength("ui_down")
+	input_steer = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+
+	var forward_dir = -transform.y    
+	var right_dir = transform.x       
+
+	var velocity_forward = linear_velocity.dot(forward_dir)
+
+	# Beschleunigung
+	if velocity_forward < max_speed:
+		var force = forward_dir * input_accel * acceleration * delta
+		apply_central_force(force)
+
+	# Lenken (abhängig von Geschwindigkeit)
+	var speed_factor = clamp(1.0 - abs(velocity_forward) / max_speed, 0.2, 1.0)
+	var steer_strength = input_steer * steering_speed * speed_factor * 1000.0  
+	apply_torque(steer_strength * sign(velocity_forward))
+
+	# Drift unterdrücken
+	var sideways_velocity = right_dir * linear_velocity.dot(right_dir)
+	linear_velocity -= sideways_velocity * grip * delta
+
+	# Downforce
+	var downforce_force = forward_dir * -1 * linear_velocity.length() * downforce * delta
+	apply_central_force(downforce_force)
+	
--- player_2.gd
+++
@@ -1,6 +0,0 @@
-extends CharacterBody2D
-@export var schneller = 100
-
-func _process(delta: float) -> void:
-	velocity = Input.get_vector("left","rechts","UP","dOWN")*schneller
-	move_and_slide()
